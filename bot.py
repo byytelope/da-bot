97b781be4ad3d862cc5af06e0dc33d694318683d
Mohamed Shadhaan <shadhanm@gmail.com>
2023-10-25 23:58:30 +0800

    ??

--- bot.py
+++ bot.py
@@ -17,8 +17,24 @@ bot = commands.Bot(intents=intents, command_prefix=".")
 
 
 @bot.event
-async def on_ready():
+async def on_ready() -> None:
     print(f"Logged in as {bot.user}")
+
+
+@bot.command()
+async def sync(ctx: commands.Context[commands.Bot]) -> None:
+    synced = await bot.tree.sync()
+    await ctx.send(f"Synced **{len(synced)}** command(s)")
+
+
+@bot.tree.command(name="blud", description="What is blud waffling about?")
+async def blud(interaction: discord.Interaction, member: discord.Member) -> None:
+    await interaction.response.send_message(f"Blud {member.mention}")
+
+
+class Bruh:
+    def __init__(self):
+        return
 
 
 bot.run(TOKEN)
